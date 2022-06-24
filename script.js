a00655fa9f434c29a9f5dab47e1f9356b696f7a0
Rizki Darmawan <rizdar.contact@gmail.com>
2022-06-24 18:38:56 +0700

    Destructuring arrays

--- script.js
+++ script.js
@@ -6,4 +6,40 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
+
+//without Destructuring
+const arr = [1, 2, 3];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+//with destructuring
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+let [first, , second] = restaurant.categories;
+console.log(first, second);
+
+// switching variables
+[first, second] = [second, first];
+console.log(first, second);
+
+//Receive 2 return values from a function
+const [starter, main] = restaurant.order(2, 0);
+console.log(starter, main);
+
+//nested
+const nested = [2, 4, [5, 6]];
+
+const [h, i, [j, k]] = nested;
+console.log(h, i, j, k);
+
+//default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
