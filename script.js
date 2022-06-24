5a601c6d5658ef42b1afdad77cdacea8bf4e1d29
Rizki Darmawan <rizdar.contact@gmail.com>
2022-06-24 19:48:54 +0700

    destructuring objects

--- script.js
+++ script.js
@@ -1,45 +1,109 @@
 'use strict';
-
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
 
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function (obj) {
+    console.log(obj);
+  },
 };
 
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                           //
+//  {} {} {}  {} {}           destructuring objects                            //
+//                                                                                                           //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const { name, categories, openingHours } = restaurant;
+console.log(name);
+console.log(categories);
+console.log(openingHours);
+
+const { name: nama, categories: kategori } = restaurant;
+
+//Default values
+const { menu = [], starterMenu: starter = [] } = restaurant;
+console.log(menu, starter);
+
+//mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+//nested objects
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                           //
+//  [][][]         destructuring arrays                            //
+//                                                                                                           //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 //without Destructuring
-const arr = [1, 2, 3];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+// const arr = [1, 2, 3];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
 
 //with destructuring
-const [x, y, z] = arr;
-console.log(x, y, z);
-console.log(arr);
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+// console.log(arr);
 
-let [first, , second] = restaurant.categories;
-console.log(first, second);
+// let [first, , second] = restaurant.categories;
+// console.log(first, second);
 
 // switching variables
-[first, second] = [second, first];
-console.log(first, second);
+// [first, second] = [second, first];
+// console.log(first, second);
 
 //Receive 2 return values from a function
-const [starter, main] = restaurant.order(2, 0);
-console.log(starter, main);
+// const [starter, main] = restaurant.order(2, 0);
+// console.log(starter, main);
 
 //nested
-const nested = [2, 4, [5, 6]];
+// const nested = [2, 4, [5, 6]];
 
-const [h, i, [j, k]] = nested;
-console.log(h, i, j, k);
+// const [h, i, [j, k]] = nested;
+// console.log(h, i, j, k);
 
 //default values
-const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r);
+// const [p = 1, q = 1, r = 1] = [8, 9];
+// console.log(p, q, r);
