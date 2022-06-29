a7ee7a3a388522625ee773c7ad1c55c70af6730c
Rizki Darmawan <rizdar.contact@gmail.com>
2022-06-29 10:47:44 +0700

    logical assignment operator

--- script.js
+++ script.js
@@ -33,9 +33,44 @@ const restaurant = {
   },
 };
 
-restaurant.numGuest = 0;
-const guest = restaurant.numGuest ?? 10;
-console.log(guest);
+const rest1 = {
+  name: 'Capri',
+  numGuest: 20,
+};
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// OR assignment operator
+// rest1.numGuest = rest1.numGuest || 10;
+// rest2.numGuest = rest2.numGuest || 10;
+// rest1.numGuest ||= 10;
+// rest2.numGuest ||= 10;
+
+//nullish assignment operator
+rest1.numGuest ??= 10;
+rest2.numGuest ??= 10;
+
+// and asssignment operator
+// rest1.owner = rest1.owner && '<ANONYMOUS';
+// rest2.owner = rest2.owner && '<ANONYMOUS';
+rest1.owner &&= '<ANONYMOUS';
+rest2.owner &&= '<ANONYMOUS';
+
+console.log(rest1);
+console.log(rest2);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                           //
+//  {} {} {}  {} {}          ??                            //
+//                                                                                                           //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// restaurant.numGuest = 0;
+// const guest = restaurant.numGuest ?? 10;
+// console.log(guest);
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
